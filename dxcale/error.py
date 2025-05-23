e39eccb33ce53ad56d8375aec6cc8f70f0dd38be
jzphlp <josephluntadilap@gmail.com>
2025-05-23 13:10:11 +0100

    Merge remote changes into utils

---
+++ dxcale/error.py
@@ -0,0 +1,10 @@
+
+use the location bbox that Rolf wanted 
+use the location lines that rolf wanted, bare location and wave thingy 
+use other location for me with line 
+
+
+given a box.gpkg read the raster to that palce 
+plot the rasters, and have RMSE at the top 
+plot the difference to the label(TDX)
+
